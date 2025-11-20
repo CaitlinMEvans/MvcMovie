dd3cd8dcc8796efdda2a3856a6cb406ed006018a
Caitlin Evans <caitmsevans@gmail.com>
2025-11-20 14:21:34 -0700

    final

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a2e4576c040a30ffde74fdcf277a35227c147b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2953ec49a516da9357750626e7e6935e7180d9c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
